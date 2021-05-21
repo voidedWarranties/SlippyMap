4b5fcd7776231488e17a0377435fe1ea53dd8537
voidedWarranties <wz68.scratch@gmail.com>
2021-05-21 02:09:59 -0700

    Make metadata public

--- SlippyMap.Game/Map.cs
+++ SlippyMap.Game/Map.cs
@@ -21,17 +21,14 @@ namespace SlippyMap.Game
 
         public readonly BindableInt ZoomLevel = new BindableInt();
 
+        public TileMetadata Metadata;
+
         public Map(IResourceStore<byte[]> store, string tilePath = "stamen-watercolor")
         {
             this.store = store;
 
             this.tilePath = tilePath;
-        }
 
-        [BackgroundDependencyLoader]
-        private void load(GameHost host)
-        {
-            var textures = new TextureStore(host.CreateTextureLoaderStore(new NamespacedResourceStore<byte[]>(store, "Maps")));
             var metadataPath = $"Maps/{tilePath}/metadata.json";
 
             using (var sw = new StreamReader(store.GetStream(metadataPath)))
@@ -41,36 +38,44 @@ namespace SlippyMap.Game
                 if (metadata == null) throw new Exception("Metadata could not be parsed");
                 if (metadata.Bounds.Count != 4) throw new Exception("Bounds array should be 4 elements long");
 
-                var topLeft = new Vector2((float)metadata.Bounds[0], (float)metadata.Bounds[3]);
-                var bottomRight = new Vector2((float)metadata.Bounds[2], (float)metadata.Bounds[1]);
+                Metadata = metadata;
+            }
+        }
 
-                var topLeftT = OsmMath.LatToTile(topLeft, metadata.MinZoom);
-                var bottomRightT = OsmMath.LatToTile(bottomRight, metadata.MinZoom);
-                var tileWidth = 256 / (1 << (metadata.MinZoom - 5));
+        [BackgroundDependencyLoader]
+        private void load(GameHost host)
+        {
+            var textures = new TextureStore(host.CreateTextureLoaderStore(new NamespacedResourceStore<byte[]>(store, "Maps")));
 
-                List<Drawable> row = new List<Drawable>();
-                List<Drawable[]> grid = new List<Drawable[]>();
+            var topLeft = new Vector2((float)Metadata.Bounds[0], (float)Metadata.Bounds[3]);
+            var bottomRight = new Vector2((float)Metadata.Bounds[2], (float)Metadata.Bounds[1]);
 
-                for (var y = (int)topLeftT.Y; y <= bottomRightT.Y; y++)
+            var topLeftT = OsmMath.LatToTile(topLeft, Metadata.MinZoom);
+            var bottomRightT = OsmMath.LatToTile(bottomRight, Metadata.MinZoom);
+            var tileWidth = 256 / (1 << (Metadata.MinZoom - 5));
+
+            List<Drawable> row = new List<Drawable>();
+            List<Drawable[]> grid = new List<Drawable[]>();
+
+            for (var y = (int)topLeftT.Y; y <= bottomRightT.Y; y++)
+            {
+                for (var x = (int)topLeftT.X; x <= bottomRightT.X; x++)
                 {
-                    for (var x = (int)topLeftT.X; x <= bottomRightT.X; x++)
+                    row.Add(new ZoomableTile(textures, new Vector2(x, y), Metadata.MinZoom, tilePath, tileWidth)
                     {
-                        row.Add(new ZoomableTile(textures, new Vector2(x, y), metadata.MinZoom, tilePath, tileWidth)
-                        {
-                            ZoomLevel = { BindTarget = ZoomLevel }
-                        });
-                    }
-
-                    grid.Add(row.ToArray());
-                    row.Clear();
+                        ZoomLevel = { BindTarget = ZoomLevel }
+                    });
                 }
 
-                InternalChild = new GridContainer
-                {
-                    Content = grid.ToArray(),
-                    Size = (bottomRightT - topLeftT + new Vector2(1)) * tileWidth
-                };
+                grid.Add(row.ToArray());
+                row.Clear();
             }
+
+            InternalChild = new GridContainer
+            {
+                Content = grid.ToArray(),
+                Size = (bottomRightT - topLeftT + new Vector2(1)) * tileWidth
+            };
         }
     }
 }
